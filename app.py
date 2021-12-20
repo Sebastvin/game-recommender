25dfa72ad49c928dcf73948e6114314c32028dd7
Sebastvin <Sebastvin@gmail.com>
2021-12-20 02:15:19 +0100

    deploying 0.0

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
-from recommendation_system import  get_recommendations
-from scraper_for_images import listUrls
+from flask import Flask, render_template, request
 import pandas as pd
+from ast import literal_eval
 pd.options.mode.chained_assignment = None
 
+metadata = pd.read_csv('workpls.csv', converters={'index': literal_eval,'result': literal_eval, 'url_links': literal_eval,
+                                                  'score': literal_eval})
+s = pd.Series(metadata.name_game)
+to_index = pd.read_csv('datasets/final_dataset.csv').loc[:, ['name_game']]
 
-app = Flask(__name__, template_folder='templates')
+urls = pd.read_csv('urls.csv', converters={'urls': literal_eval}, index_col=0)
+
+
+app = Flask(__name__)
+
+
+def toUrls(indexes, urls):
+    data = []
+    for idx in indexes[0]:
+        if len(urls.to_numpy()[idx, 0]) > 0:
+            data.append(urls.to_numpy()[idx, 0][0])
+
+    return data
+
+
 
 @app.route('/')
 def home():
@@ -14,17 +31,27 @@ def home():
 
 @app.route('/content',  methods=['GET', 'POST'])
 def get_data():
+    def toUrls(indexes, urls):
+        data = []
+        for idx in indexes[0]:
+            if len(urls.to_numpy()[idx, 0]) > 0:
+                data.append(urls.to_numpy()[idx, 0][0])
+
+        return data
+
     if request.method == 'POST' or request.method == 'GET':
         user = request.form['search']
-        game = get_recommendations(str(user))
-        des = game.iloc[:, 1].values
-        score = game.iloc[:, 2].tolist()
-        link = game.iloc[:, 6].tolist()
-        index =  game.iloc[:, 0].tolist()
-        root = "Recommendations for "
-        return render_template('content.html', game=des, name= root + str(user), des=des, urlsImg=des, score=score,
-                               link=link, index=index)
+
+        if user in s.values:
+            index = metadata[metadata.name_game == user].iloc[:, 0].to_list()
+            links = toUrls(index, urls)
+            game = metadata[metadata.name_game == user].iloc[:, 2].to_list()[0]
+            link = metadata[metadata.name_game == user].iloc[:, 3].to_list()[0]
+            score = metadata[metadata.name_game == user].iloc[:, 4].to_list()[0]
+            return render_template('content.html', game=game, link=link, score=score, user=user, links=links)
+        else:
+            return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(threaded=True, port=5000)
+    app.run()
