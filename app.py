be0db16b02f39287ca5216324a1416e6f6995547
Sebastvin <Sebastvin@gmail.com>
2021-12-22 11:18:31 +0100

    polish

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import pandas as pd
 from ast import literal_eval
 pd.options.mode.chained_assignment = None
 
-metadata = pd.read_csv('workpls.csv', converters={'index': literal_eval,'result': literal_eval, 'url_links': literal_eval,
+metadata = pd.read_csv('datasets/data_to_show_app.csv', converters={'index': literal_eval, 'result': literal_eval, 'url_links': literal_eval,
                                                   'score': literal_eval})
 s = pd.Series(metadata.name_game)
 to_index = pd.read_csv('datasets/final_dataset.csv').loc[:, ['name_game']]
 
-urls = pd.read_csv('urls.csv', converters={'urls': literal_eval}, index_col=0)
+urls = pd.read_csv('datasets/urls.csv', converters={'urls': literal_eval}, index_col=0)
 
 
 app = Flask(__name__)
 
-
-def toUrls(indexes, urls):
+# Method returns list of links to images
+def getUrls(indexes, urls):
     data = []
     for idx in indexes[0]:
         if len(urls.to_numpy()[idx, 0]) > 0:
@@ -23,34 +23,31 @@ def toUrls(indexes, urls):
     return data
 
 
-
+# Main page
 @app.route('/')
 def home():
-    return (render_template('index.html'))
+    return render_template('index.html')
 
 
+@app.route('/error')
+def bad_value():
+    return render_template('error.html')
+
+# Page with results
 @app.route('/content',  methods=['GET', 'POST'])
 def get_data():
-    def toUrls(indexes, urls):
-        data = []
-        for idx in indexes[0]:
-            if len(urls.to_numpy()[idx, 0]) > 0:
-                data.append(urls.to_numpy()[idx, 0][0])
-
-        return data
-
     if request.method == 'POST' or request.method == 'GET':
         user = request.form['search']
 
         if user in s.values:
             index = metadata[metadata.name_game == user].iloc[:, 0].to_list()
-            links = toUrls(index, urls)
+            links = getUrls(index, urls)
             game = metadata[metadata.name_game == user].iloc[:, 2].to_list()[0]
             link = metadata[metadata.name_game == user].iloc[:, 3].to_list()[0]
             score = metadata[metadata.name_game == user].iloc[:, 4].to_list()[0]
             return render_template('content.html', game=game, link=link, score=score, user=user, links=links)
         else:
-            return render_template('index.html')
+          return render_template('error.html')
 
 
 if __name__ == "__main__":
